3f3a6ab5f152f935ad934236d4f032ff6f0d02c2
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2025-11-25 13:15:18 -0800

    fix: update vercel schema

--- server/api/vercel/[channelid].post.ts
+++ server/api/vercel/[channelid].post.ts
@@ -4,55 +4,57 @@ import dayjs from "dayjs";
 export default defineEventHandler(async (event) => {
   const { channelid } = event.context.params as { channelid: string };
 
-  const bodySchema = z.object({
-    id: z.string(),
-    deploymentId: z.string(),
-    source: z.string(),
-    host: z.string(),
-    timestamp: z.bigint(),
-    projectId: z.string(),
-    level: z.string(),
-    message: z.string().optional(),
-    buildId: z.string().optional(),
-    entrypoint: z.string().optional(),
-    destination: z.string().optional(),
-    path: z.string().optional(),
-    type: z.string().optional(),
-    statusCode: z.number().optional(),
-    requestId: z.string().optional(),
-    environment: z.string().optional(),
-    branch: z.string().optional(),
-    ja3Digest: z.string().optional(),
-    ja4Digest: z.string().optional(),
-    edgeType: z.string().optional(),
-    projectName: z.string().optional(),
-    executionRegion: z.string().optional(),
-    traceId: z.string().optional(),
-    spanId: z.string().optional(),
-    proxy: z
-      .object({
-        timestamp: z.bigint(),
-        method: z.string(),
-        host: z.string(),
-        path: z.string(),
-        userAgent: z.string(),
-        region: z.string(),
-        referer: z.string().optional(),
-        statusCode: z.number().optional(),
-        clientIp: z.string().optional(),
-        scheme: z.string().optional(),
-        responseByteSize: z.number().optional(),
-        cacheId: z.string().optional(),
-        pathType: z.string().optional(),
-        pathTypeVariant: z.string().optional(),
-        vercelId: z.string().optional(),
-        vercelCache: z.string().optional(),
-        lambdaRegion: z.string().optional(),
-        wafAction: z.string().optional(),
-        wafRuleId: z.string().optional(),
-      })
-      .optional(),
-  });
+  const bodySchema = z.array(
+    z.object({
+      id: z.string(),
+      deploymentId: z.string(),
+      source: z.string(),
+      host: z.string(),
+      timestamp: z.bigint(),
+      projectId: z.string(),
+      level: z.string(),
+      message: z.string().optional(),
+      buildId: z.string().optional(),
+      entrypoint: z.string().optional(),
+      destination: z.string().optional(),
+      path: z.string().optional(),
+      type: z.string().optional(),
+      statusCode: z.number().optional(),
+      requestId: z.string().optional(),
+      environment: z.string().optional(),
+      branch: z.string().optional(),
+      ja3Digest: z.string().optional(),
+      ja4Digest: z.string().optional(),
+      edgeType: z.string().optional(),
+      projectName: z.string().optional(),
+      executionRegion: z.string().optional(),
+      traceId: z.string().optional(),
+      spanId: z.string().optional(),
+      proxy: z
+        .object({
+          timestamp: z.bigint(),
+          method: z.string(),
+          host: z.string(),
+          path: z.string(),
+          userAgent: z.string(),
+          region: z.string(),
+          referer: z.string().optional(),
+          statusCode: z.number().optional(),
+          clientIp: z.string().optional(),
+          scheme: z.string().optional(),
+          responseByteSize: z.number().optional(),
+          cacheId: z.string().optional(),
+          pathType: z.string().optional(),
+          pathTypeVariant: z.string().optional(),
+          vercelId: z.string().optional(),
+          vercelCache: z.string().optional(),
+          lambdaRegion: z.string().optional(),
+          wafAction: z.string().optional(),
+          wafRuleId: z.string().optional(),
+        })
+        .optional(),
+    }),
+  );
 
   const body = await readBody(event);
 
@@ -78,25 +80,29 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const level = parsedBody.data.level || "info";
-  const message = JSON.stringify(parsedBody.data) || "{}";
+  const logs = parsedBody.data.map(async (l) => {
+    const level = l.level || "info";
+    const message = JSON.stringify(l) || "{}";
 
-  const log = await prisma.log.create({
-    data: {
-      level,
-      message,
-      type: "json",
-      thread: -1,
-      channel_id: channelid,
-    },
+    const log = await prisma.log.create({
+      data: {
+        level,
+        message,
+        type: "json",
+        thread: -1,
+        channel_id: channelid,
+      },
+    });
+
+    if (!log) {
+      throw createError({
+        message: "An error occurred while creating the log",
+        statusCode: 500,
+      });
+    }
   });
 
-  if (!log) {
-    throw createError({
-      message: "An error occurred while creating the log",
-      statusCode: 500,
-    });
-  }
+  console.log("Created logs:", logs.length);
 
   // hacks
   // Delete expired logs. Only run with a random chance of 5%
